b64e24d4eab8652875a8b3c65cc926441d837b25
Yashwanth <yashwanterla@gmail.com>
2026-07-31 15:07:36 +0530

    Add files via upload

---
+++ input 7.py
@@ -0,0 +1,14 @@
+class solution:
+    def climbstairs(self,n):
+        if n <= 2:
+            return n
+        first = 1
+        second = 2
+        for i in range(3, n+1):
+            third = first + second
+            first = second 
+            second = third
+        return second
+n = 7
+obj = solution()
+print(obj.climbstairs(n))
